f07bb90f25eb4432241fcea3496d6e5faff990dd
Marquine <lmarquine@gmail.com>
2023-05-28 20:46:02 -0300

    Update vendor name

--- registration.php
+++ registration.php
@@ -1,7 +1,7 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'mageup_pt_br',
+    'Web2view_pt_br',
     __DIR__
 );
 
